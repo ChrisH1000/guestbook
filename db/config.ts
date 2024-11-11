00fb3bd36da215685362439273db1b3cba3ca758
Chris Hill <christopher.hill@ecu.edu.au>
2024-11-11 09:06:24 +0800

    feat: setup db and seeds

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,15 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column, NOW } from 'astro:db'
+
+const GuestBook = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    author: column.text(),
+    content: column.text(),
+    timestamp: column.date({ default: NOW }),
+  }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { GuestBook},
 });
