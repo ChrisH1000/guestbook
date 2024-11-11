fd0efa15bf089f6cc2229ab18c84be11e14c791d
Chris Hill <christopher.hill@ecu.edu.au>
2024-11-11 09:06:34 +0800

    feat: added server mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import db from '@astrojs/db';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [db()]
+  output: 'server',
+  integrations: [db()],
+  adapter: netlify()
 });
